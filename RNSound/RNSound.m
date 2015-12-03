0598150013733200d877fa21abb15b53fe20cf08
Zhen Wang <z@zmxv.com>
2015-12-02 20:33:22 -0800

    Redesign the JS/Obj-C API to enable control of individual audio player

--- RNSound/RNSound.m
+++ RNSound/RNSound.m
@@ -3,65 +3,62 @@
 
 
 @implementation RNSound {
-  NSMutableArray* _playerPool;
-  BOOL _enabled;
+  NSMutableDictionary* _playerPool;
 }
 
--(NSMutableArray*) playerPool {
+-(NSMutableDictionary*) playerPool {
   if (!_playerPool) {
-    _playerPool = [[NSMutableArray alloc] init];
+    _playerPool = [[NSMutableDictionary alloc] init];
   }
   return _playerPool;
 }
 
--(NSURL*) soundURL:(NSString*)fileName {
-  return [[NSBundle mainBundle] URLForResource:[[fileName lastPathComponent]stringByDeletingPathExtension]
-                                 withExtension:[fileName pathExtension]];
+-(AVAudioPlayer*) playerForKey:(nonnull NSNumber*)key {
+  return [[self playerPool] objectForKey:key];
 }
 
 RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(enable:(BOOL)enabled) {
-  _enabled = enabled;
   AVAudioSession *session = [AVAudioSession sharedInstance];
   [session setCategory: AVAudioSessionCategoryAmbient error: nil];
   [session setActive: enabled error: nil];
 }
 
-RCT_EXPORT_METHOD(prepare:(NSString *)fileName) {
-  AVAudioPlayer* player = [[AVAudioPlayer alloc] initWithContentsOfURL:[self soundURL:fileName] error:nil];
+RCT_EXPORT_METHOD(prepare:(NSString*)fileName withKey:(nonnull NSNumber*)key) {
+  AVAudioPlayer* player = [[AVAudioPlayer alloc]
+                           initWithContentsOfURL:[NSURL fileURLWithPath:fileName] error:nil];
   [player prepareToPlay];
-  [[self playerPool] addObject:player];
+  [[self playerPool] setObject:player forKey:key];
 }
 
-RCT_EXPORT_METHOD(play:(NSString *)fileName) {
-  if (!_enabled) {
-    return;
+RCT_EXPORT_METHOD(play:(nonnull NSNumber*)key) {
+  AVAudioPlayer* player = [self playerForKey:key];
+  if (player) {
+    [player play];
   }
-  
-  NSURL *soundURL = [self soundURL:fileName];
-  
-  for (AVAudioPlayer* player in [[self playerPool] mutableCopy]) {
-    if (!player.playing && [player.url isEqual:soundURL]) {
-      [player play];
-      return;
-    }
-  }
-  
-  AVAudioPlayer* player = [[AVAudioPlayer alloc] initWithContentsOfURL:soundURL error:nil];
-  [player play];
-  [[self playerPool] addObject:player];
 }
 
-RCT_EXPORT_METHOD(stop:(NSString *)fileName) {
-    NSURL *soundURL = [self soundURL:fileName];
-    
-    for (AVAudioPlayer* player in [[self playerPool] mutableCopy]) {
-        if (player.playing && [player.url isEqual:soundURL]) {
-            [player stop];
-            return;
-        }
-    }
+RCT_EXPORT_METHOD(pause:(nonnull NSNumber*)key) {
+  AVAudioPlayer* player = [self playerForKey:key];
+  if (player) {
+    [player pause];
+  }
+}
+
+RCT_EXPORT_METHOD(stop:(nonnull NSNumber*)key) {
+  AVAudioPlayer* player = [self playerForKey:key];
+  if (player) {
+    [player stop];
+  }
+}
+
+RCT_EXPORT_METHOD(release:(nonnull NSNumber*)key) {
+  AVAudioPlayer* player = [self playerForKey:key];
+  if (player) {
+    [player stop];
+    [[self playerPool] removeObjectForKey:key];
+  }
 }
 
 @end
--- sound.js
+++ sound.js
@@ -1,1 +1,35 @@
-module.exports = require('react-native').NativeModules.RNSound;
+'use strict';
+
+var RNSound = require('react-native').NativeModules.RNSound;
+
+var nextKey = 0;
+
+function Sound(filename) {
+  this.filename = filename;
+  this.key = nextKey++;
+  RNSound.prepare(filename, this.key);
+}
+
+Sound.prototype.play = function() {
+  RNSound.play(this.key);
+};
+
+Sound.prototype.pause = function() {
+  RNSound.pause(this.key);
+};
+
+Sound.prototype.stop = function() {
+  RNSound.stop(this.key);
+};
+
+Sound.prototype.release = function() {
+  RNSound.release(this.key);
+};
+
+Sound.enable = function(enabled) {
+  RNSound.enable(enabled);
+};
+
+Sound.enable(true);
+
+module.exports = Sound;
