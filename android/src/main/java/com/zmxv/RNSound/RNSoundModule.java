ee04b67deb328f586016e66748a6e2fd4a453cfa
Zhen Wang <z@zmxv.com>
2015-12-23 15:26:40 -0800

    Basic playback support for Android

---
+++ android/src/main/java/com/zmxv/RNSound/RNSoundModule.java
@@ -0,0 +1,74 @@
+package com.zmxv.RNSound;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.WritableMap;
+
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.media.MediaPlayer.OnErrorListener;
+
+public class RNSoundModule extends ReactContextBaseJavaModule {
+  Map<Integer, MediaPlayer> playerPool = new HashMap<>();
+  ReactApplicationContext context;
+  final static Object NULL = null;
+
+  public RNSoundModule(ReactApplicationContext context) {
+    super(context);
+    this.context = context;
+  }
+
+  @Override
+  public String getName() {
+    return "RNSound";
+  }
+
+  @ReactMethod
+  public void enable(final Boolean enabled) {
+    // no-op
+  }
+
+  @ReactMethod
+  public void prepare(final String fileName, final Integer key, final Callback callback) {
+    int res = this.context.getResources().getIdentifier(fileName, "raw", this.context.getPackageName());
+    if (res == 0) {
+      WritableMap e = Arguments.createMap();
+      e.putInt("code", -1);
+      e.putString("message", "resource not found");
+      callback.invoke(e);
+      return;
+    }
+    MediaPlayer player = MediaPlayer.create(this.context, res);
+    this.playerPool.put(key, player);
+    callback.invoke(NULL);
+  }
+
+  @ReactMethod
+  public void play(final Integer key, final Callback callback) {
+    MediaPlayer player = this.playerPool.get(key);
+    if (player == null) {
+      callback.invoke(false);
+      return;
+    }
+    player.setOnCompletionListener(new OnCompletionListener() {
+      @Override
+      public void onCompletion(MediaPlayer mp) {
+        callback.invoke(true);
+      }
+    });
+    player.setOnErrorListener(new OnErrorListener() {
+      @Override
+      public boolean onError(MediaPlayer mp, int what, int extra) {
+        callback.invoke(false);
+        return true;
+      }
+    });
+    player.start();
+  }
+}
