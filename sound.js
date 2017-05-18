94e086507c83f67970f2ebc185266ec1aa48b111
kimxogus <kgyoo8232@gmail.com>
2017-05-18 19:39:18 +0900

    remove checking if loaded

--- sound.js
+++ sound.js
@@ -118,10 +118,8 @@ Sound.prototype.getSystemVolume = function(callback) {
 };
 
 Sound.prototype.setSystemVolume = function(value) {
-  if (this._loaded) {
-    if (IsAndroid) {
-      RNSound.setSystemVolume(value);
-    }
+  if (IsAndroid) {
+    RNSound.setSystemVolume(value);
   }
   return this;
 };
