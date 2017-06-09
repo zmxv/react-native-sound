f489895263af8bf9de126b02d39aa5731acc14c8
Fabian Cook <faybecook@gmail.com>
2017-06-09 20:11:55 +1200

    Prepare MediaPlayer async for Android

--- android/src/main/java/com/zmxv/RNSound/RNSoundModule.java
+++ android/src/main/java/com/zmxv/RNSound/RNSoundModule.java
@@ -45,10 +45,23 @@ public class RNSoundModule extends ReactContextBaseJavaModule {
       callback.invoke(e);
       return;
     }
-    this.playerPool.put(key, player);
-    WritableMap props = Arguments.createMap();
-    props.putDouble("duration", player.getDuration() * .001);
-    callback.invoke(NULL, props);
+    final RNSoundModule module = this;
+    player.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+      boolean callbackWasCalled = false;
+
+      @Override
+      public synchronized void onPrepared(MediaPlayer mp) {
+        if (callbackWasCalled) return;
+        callbackWasCalled = true;
+
+        module.playerPool.put(key, mp);
+        WritableMap props = Arguments.createMap();
+        props.putDouble("duration", mp.getDuration() * .001);
+        callback.invoke(NULL, props);
+      }
+
+    });
+    player.prepareAsync();
   }
 
   protected MediaPlayer createMediaPlayer(final String fileName) {
