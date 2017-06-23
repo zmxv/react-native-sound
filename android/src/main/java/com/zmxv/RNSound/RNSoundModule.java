71b16def0d219a0befcc568d5844fb2bf9ae2a32
Marcin Pawlowski <marcin.pawlowski@netbulls.eu>
2017-06-23 14:51:00 +0200

    Change formatting

--- android/src/main/java/com/zmxv/RNSound/RNSoundModule.java
+++ android/src/main/java/com/zmxv/RNSound/RNSoundModule.java
@@ -22,280 +22,272 @@ import java.io.IOException;
 import android.util.Log;
 
 public class RNSoundModule extends ReactContextBaseJavaModule {
-	Map<Integer, MediaPlayer> playerPool = new HashMap<>();
-	ReactApplicationContext context;
-	final static Object NULL = null;
-	/**
-	 * Number of loops for the sound.
-	 * -1 - play inifity
-	 * 0 - play once
-	 * 1 - play twice etc.
-	 */
-	Integer loops = 0;
+  Map<Integer, MediaPlayer> playerPool = new HashMap<>();
+  ReactApplicationContext context;
+  final static Object NULL = null;
+  /**
+   * Number of loops for the sound.
+   * -1 - play inifity
+   * 0 - play once
+   * 1 - play twice etc.
+   */
+  Integer loops = 0;
 
-	public RNSoundModule(ReactApplicationContext context) {
-		super(context);
-		this.context = context;
-	}
+  public RNSoundModule(ReactApplicationContext context) {
+    super(context);
+    this.context = context;
+  }
 
-	@Override
-	public String getName() {
-		return "RNSound";
-	}
+  @Override
+  public String getName() {
+    return "RNSound";
+  }
 
-	@ReactMethod
-	public void prepare(final String fileName, final Integer key, final ReadableMap options, final Callback callback) {
-		MediaPlayer player = createMediaPlayer(fileName);
-		if (player == null) {
-			WritableMap e = Arguments.createMap();
-			e.putInt("code", -1);
-			e.putString("message", "resource not found");
-			return;
-		}
+  @ReactMethod
+  public void prepare(final String fileName, final Integer key, final ReadableMap options, final Callback callback) {
+    MediaPlayer player = createMediaPlayer(fileName);
+    if (player == null) {
+      WritableMap e = Arguments.createMap();
+      e.putInt("code", -1);
+      e.putString("message", "resource not found");
+      return;
+    }
 
-		final RNSoundModule module = this;
+    final RNSoundModule module = this;
 
-		player.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-			boolean callbackWasCalled = false;
+    player.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+      boolean callbackWasCalled = false;
 
-			@Override
-			public synchronized void onPrepared(MediaPlayer mp) {
-				if (callbackWasCalled)
-					return;
-				callbackWasCalled = true;
+      @Override
+      public synchronized void onPrepared(MediaPlayer mp) {
+        if (callbackWasCalled) return;
+        callbackWasCalled = true;
 
-				module.playerPool.put(key, mp);
-				WritableMap props = Arguments.createMap();
-				props.putDouble("duration", mp.getDuration() * .001);
-				try {
-					callback.invoke(NULL, props);
-				}
-				catch (RuntimeException runtimeException) {
-					// The callback was already invoked
-					Log.e("RNSoundModule", "Exception", runtimeException);
-				}
-			}
+        module.playerPool.put(key, mp);
+        WritableMap props = Arguments.createMap();
+        props.putDouble("duration", mp.getDuration() * .001);
+        try {
+          callback.invoke(NULL, props);
+        } catch(RuntimeException runtimeException) {
+          // The callback was already invoked
+          Log.e("RNSoundModule", "Exception", runtimeException);
+        }
+      }
 
-		});
+    });
 
-		player.setOnErrorListener(new OnErrorListener() {
-			boolean callbackWasCalled = false;
+    player.setOnErrorListener(new OnErrorListener() {
+      boolean callbackWasCalled = false;
 
-			@Override
-			public synchronized boolean onError(MediaPlayer mp, int what, int extra) {
-				if (callbackWasCalled)
-					return true;
-				callbackWasCalled = true;
-				try {
-					WritableMap props = Arguments.createMap();
-					props.putInt("what", what);
-					props.putInt("extra", extra);
-					callback.invoke(props, NULL);
-				}
-				catch (RuntimeException runtimeException) {
-					// The callback was already invoked
-					Log.e("RNSoundModule", "Exception", runtimeException);
-				}
-				return true;
-			}
-		});
+      @Override
+      public synchronized boolean onError(MediaPlayer mp, int what, int extra) {
+        if (callbackWasCalled) return true;
+        callbackWasCalled = true;
+        try {
+          WritableMap props = Arguments.createMap();
+          props.putInt("what", what);
+          props.putInt("extra", extra);
+          callback.invoke(props, NULL);
+        } catch(RuntimeException runtimeException) {
+          // The callback was already invoked
+          Log.e("RNSoundModule", "Exception", runtimeException);
+        }
+        return true;
+      }
+    });
 
-		try {
-			player.prepareAsync();
-		}
-		catch (IllegalStateException ignored) {
-			// When loading files from a file, we useMediaPlayer.create, which actually
-			// prepares the audio for us already. So we catch and ignore this error
-		}
-	}
+    try {
+      player.prepareAsync();
+    } catch (IllegalStateException ignored) {
+      // When loading files from a file, we useMediaPlayer.create, which actually
+      // prepares the audio for us already. So we catch and ignore this error
+    }
+  }
 
-	protected MediaPlayer createMediaPlayer(final String fileName) {
-		int res = this.context.getResources().getIdentifier(fileName, "raw", this.context.getPackageName());
-		if (res != 0) {
-			return MediaPlayer.create(this.context, res);
-		}
-		if (fileName.startsWith("http://") || fileName.startsWith("https://")) {
-			MediaPlayer mediaPlayer = new MediaPlayer();
-			mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-			Log.i("RNSoundModule", fileName);
-			try {
-				mediaPlayer.setDataSource(fileName);
-			}
-			catch (IOException e) {
-				Log.e("RNSoundModule", "Exception", e);
-				return null;
-			}
-			return mediaPlayer;
-		}
+  protected MediaPlayer createMediaPlayer(final String fileName) {
+    int res = this.context.getResources().getIdentifier(fileName, "raw", this.context.getPackageName());
+    if (res != 0) {
+      return MediaPlayer.create(this.context, res);
+    }
+    if(fileName.startsWith("http://") || fileName.startsWith("https://")) {
+      MediaPlayer mediaPlayer = new MediaPlayer();
+      mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+      Log.i("RNSoundModule", fileName);
+      try {
+        mediaPlayer.setDataSource(fileName);
+      } catch(IOException e) {
+        Log.e("RNSoundModule", "Exception", e);
+        return null;
+      }
+      return mediaPlayer;
+    }
 
-		File file = new File(fileName);
-		if (file.exists()) {
-			Uri uri = Uri.fromFile(file);
-			// Mediaplayer is already prepared here.
-			return MediaPlayer.create(this.context, uri);
-		}
-		return null;
-	}
+    File file = new File(fileName);
+    if (file.exists()) {
+      Uri uri = Uri.fromFile(file);
+      // Mediaplayer is already prepared here.
+      return MediaPlayer.create(this.context, uri);
+    }
+    return null;
+  }
 
-	@ReactMethod
-	public void play(final Integer key, final Callback callback) {
-		MediaPlayer player = this.playerPool.get(key);
-		if (player == null) {
-			callback.invoke(false);
-			return;
-		}
-		if (player.isPlaying()) {
-			return;
-		}
-		player.setOnCompletionListener(new OnCompletionListenerLooped(callback, this.loops));
-		player.setOnErrorListener(new OnErrorListener() {
-			boolean callbackWasCalled = false;
+  @ReactMethod
+  public void play(final Integer key, final Callback callback) {
+    MediaPlayer player = this.playerPool.get(key);
+    if (player == null) {
+      callback.invoke(false);
+      return;
+    }
+    if (player.isPlaying()) {
+      return;
+    }
+    player.setOnCompletionListener(new OnCompletionListenerLooped(callback, this.loops));
+    player.setOnErrorListener(new OnErrorListener() {
+      boolean callbackWasCalled = false;
 
-			@Override
-			public synchronized boolean onError(MediaPlayer mp, int what, int extra) {
-				if (callbackWasCalled)
-					return true;
-				callbackWasCalled = true;
-				callback.invoke(false);
-				return true;
-			}
-		});
-		player.start();
-	}
+      @Override
+      public synchronized boolean onError(MediaPlayer mp, int what, int extra) {
+        if (callbackWasCalled) return true;
+        callbackWasCalled = true;
+        callback.invoke(false);
+        return true;
+      }
+    });
+    player.start();
+  }
 
-	@ReactMethod
-	public void pause(final Integer key, final Callback callback) {
-		MediaPlayer player = this.playerPool.get(key);
-		if (player != null && player.isPlaying()) {
-			player.pause();
-		}
-		callback.invoke();
-	}
+  @ReactMethod
+  public void pause(final Integer key, final Callback callback) {
+    MediaPlayer player = this.playerPool.get(key);
+    if (player != null && player.isPlaying()) {
+      player.pause();
+    }
+    callback.invoke();
+  }
 
-	@ReactMethod
-	public void stop(final Integer key, final Callback callback) {
-		MediaPlayer player = this.playerPool.get(key);
-		if (player != null && player.isPlaying()) {
-			player.pause();
-			player.seekTo(0);
-		}
-		callback.invoke();
-	}
+  @ReactMethod
+  public void stop(final Integer key, final Callback callback) {
+    MediaPlayer player = this.playerPool.get(key);
+    if (player != null && player.isPlaying()) {
+      player.pause();
+      player.seekTo(0);
+    }
+    callback.invoke();
+  }
 
-	@ReactMethod
-	public void release(final Integer key) {
-		MediaPlayer player = this.playerPool.get(key);
+  @ReactMethod
+  public void release(final Integer key) {
+    MediaPlayer player = this.playerPool.get(key);
+    if (player != null) {
+      player.release();
+      this.playerPool.remove(key);
+    }
+  }
+
+  @ReactMethod
+  public void setVolume(final Integer key, final Float left, final Float right) {
+    MediaPlayer player = this.playerPool.get(key);
+    if (player != null) {
+      player.setVolume(left, right);
+    }
+  }
+
+  @ReactMethod
+  public void setNumberOfLoops(final Integer key, final Integer loops) {
+  	if (loops == -1) {
+  	  MediaPlayer player = this.playerPool.get(key);
 		if (player != null) {
-			player.release();
-			this.playerPool.remove(key);
+		  player.setLooping(true);
 		}
 	}
-
-	@ReactMethod
-	public void setVolume(final Integer key, final Float left, final Float right) {
-		MediaPlayer player = this.playerPool.get(key);
-		if (player != null) {
-			player.setVolume(left, right);
-		}
+	else {
+  	  this.loops = loops;
 	}
+  }
 
-	@ReactMethod
-	public void setNumberOfLoops(final Integer key, final Integer loops) {
-		if (loops == -1) {
-			MediaPlayer player = this.playerPool.get(key);
-			if (player != null) {
-				player.setLooping(true);
-			}
-		}
-		else {
-			this.loops = loops;
-		}
-	}
+  @ReactMethod
+  public void setSpeed(final Integer key, final Float speed) {
+    MediaPlayer player = this.playerPool.get(key);
+    if (player != null) {
+      player.setPlaybackParams(player.getPlaybackParams().setSpeed(speed));
+    }
+  }
 
-	@ReactMethod
-	public void setSpeed(final Integer key, final Float speed) {
-		MediaPlayer player = this.playerPool.get(key);
-		if (player != null) {
-			player.setPlaybackParams(player.getPlaybackParams().setSpeed(speed));
-		}
-	}
+  @ReactMethod
+  public void setCurrentTime(final Integer key, final Float sec) {
+    MediaPlayer player = this.playerPool.get(key);
+    if (player != null) {
+      player.seekTo((int)Math.round(sec * 1000));
+    }
+  }
 
-	@ReactMethod
-	public void setCurrentTime(final Integer key, final Float sec) {
-		MediaPlayer player = this.playerPool.get(key);
-		if (player != null) {
-			player.seekTo((int) Math.round(sec * 1000));
-		}
-	}
+  @ReactMethod
+  public void getCurrentTime(final Integer key, final Callback callback) {
+    MediaPlayer player = this.playerPool.get(key);
+    if (player == null) {
+      callback.invoke(-1, false);
+      return;
+    }
+    callback.invoke(player.getCurrentPosition() * .001, player.isPlaying());
+  }
 
-	@ReactMethod
-	public void getCurrentTime(final Integer key, final Callback callback) {
-		MediaPlayer player = this.playerPool.get(key);
-		if (player == null) {
-			callback.invoke(-1, false);
-			return;
-		}
-		callback.invoke(player.getCurrentPosition() * .001, player.isPlaying());
-	}
+  //turn speaker on
+  @ReactMethod
+  public void setSpeakerphoneOn(final Integer key, final Boolean speaker) {
+    MediaPlayer player = this.playerPool.get(key);
+    if (player != null) {
+      player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+      AudioManager audioManager = (AudioManager)this.context.getSystemService(this.context.AUDIO_SERVICE);
+      audioManager.setMode(AudioManager.MODE_IN_COMMUNICATION);
+      audioManager.setSpeakerphoneOn(speaker);
+    }
+  }
 
-	//turn speaker on
-	@ReactMethod
-	public void setSpeakerphoneOn(final Integer key, final Boolean speaker) {
-		MediaPlayer player = this.playerPool.get(key);
-		if (player != null) {
-			player.setAudioStreamType(AudioManager.STREAM_MUSIC);
-			AudioManager audioManager = (AudioManager) this.context.getSystemService(this.context.AUDIO_SERVICE);
-			audioManager.setMode(AudioManager.MODE_IN_COMMUNICATION);
-			audioManager.setSpeakerphoneOn(speaker);
-		}
-	}
+  @ReactMethod
+  public void enable(final Boolean enabled) {
+    // no op
+  }
 
-	@ReactMethod
-	public void enable(final Boolean enabled) {
-		// no op
-	}
-
-	@Override
-	public Map<String, Object> getConstants() {
-		final Map<String, Object> constants = new HashMap<>();
-		constants.put("IsAndroid", true);
-		return constants;
-	}
+  @Override
+  public Map<String, Object> getConstants() {
+    final Map<String, Object> constants = new HashMap<>();
+    constants.put("IsAndroid", true);
+    return constants;
+  }
 }
 
 class OnCompletionListenerLooped implements OnCompletionListener {
-	/**
-	 * Number of loops. 0 - play once, 1 - play twice etc.
-	 * Media player have set looping to true if -1 is given from react component.
-	 */
-	Integer loops = 0;
-	boolean callbackWasCalled = false;
-	Callback callback;
+  /**
+   * Number of loops. 0 - play once, 1 - play twice etc.
+   * Media player have set looping to true if -1 is given from react component.
+   */
+  Integer loops = 0;
+  boolean callbackWasCalled = false;
+  Callback callback;
 
-	public OnCompletionListenerLooped(Callback callback, Integer loops) {
-		super();
-		this.callback = callback;
-		this.loops = loops;
+  public OnCompletionListenerLooped(Callback callback, Integer loops) {
+  	super();
+  	this.callback = callback;
+  	this.loops = loops;
+  }
+
+  @Override
+  public synchronized void onCompletion(MediaPlayer mp) {
+  	if (!mp.isLooping()) {
+  	  if (this.loops > 0) {
+  	  	mp.start();
+  	  	this.loops--;
+  	  	return;
+	  }
+	  if (callbackWasCalled)
+	  	return;
+  	  callbackWasCalled = true;
+  	  try {
+  	  	callback.invoke(true);
+	  }
+	  catch (Exception e) {
+  	  	//Catches the exception: java.lang.RuntimeException·Illegal callback invocation from native module
+	  }
 	}
-
-	@Override
-	public synchronized void onCompletion(MediaPlayer mp) {
-		if (!mp.isLooping()) {
-			if (this.loops > 0) {
-				mp.start();
-				this.loops--;
-				return;
-			}
-
-			if (callbackWasCalled)
-				return;
-			callbackWasCalled = true;
-			try {
-				callback.invoke(true);
-			}
-			catch (Exception e) {
-				//Catches the exception: java.lang.RuntimeException·Illegal callback invocation from native module
-			}
-		}
-	}
+  }
 }
