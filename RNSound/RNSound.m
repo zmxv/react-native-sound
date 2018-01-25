a58d4cf765b405a308a74242dee1a434b4a43420
Davy CHHOUK <chhouk.davy@gmail.com>
2018-01-25 17:13:25 +0700

    A quick fix for iOS release build.
    The sound does not work on iOS release build.
    @Reference: https://github.com/zmxv/react-native-sound/issues/321

--- RNSound/RNSound.m
+++ RNSound/RNSound.m
@@ -191,7 +191,7 @@ RCT_EXPORT_METHOD(prepare:(NSString*)fileName
     player = [[AVAudioPlayer alloc] initWithContentsOfURL:fileNameUrl error:&error];
   }
   else {
-    fileNameUrl = [NSURL fileURLWithPath:[fileName stringByRemovingPercentEncoding]];
+    fileNameUrl = [NSURL URLWithString: fileName];
     player = [[AVAudioPlayer alloc]
               initWithContentsOfURL:fileNameUrl
               error:&error];
