7003542a1b84ee52c0592c3e23bbad3e5c457c08
Vinod Reddy Rondla <vinod@kenandy.com>
2018-04-16 14:51:39 -0700

    Backward compatibility.

--- RNSound/RNSound.h
+++ RNSound/RNSound.h
@@ -5,7 +5,12 @@
 #endif
 
 #import <AVFoundation/AVFoundation.h>
+
+#if __has_include(<React/RCTEventEmitter.h>)
 #import <React/RCTEventEmitter.h>
+#else
+#import "RCTEventEmitter.h"
+#endif
 
 @interface RNSound : RCTEventEmitter <RCTBridgeModule, AVAudioPlayerDelegate>
 @property (nonatomic, weak) NSNumber* _key;
