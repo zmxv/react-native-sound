b04201924024abc3c6b6922887c32c2da18417b9
jvb <johan.vanderboom@tomtom.com>
2022-02-24 17:35:22 +0100

    feat(DDAPP-11444): set mode VoicePrompt when connected to CarPlay

--- RNSound/RNSound.h
+++ RNSound/RNSound.h
@@ -15,4 +15,5 @@
 @interface RNSound : RCTEventEmitter <RCTBridgeModule, AVAudioPlayerDelegate>
 @property (nonatomic, weak) NSNumber *_key;
 @property (nonatomic) BOOL useAudioSession;
+@property (nonatomic) BOOL carPlay;
 @end
