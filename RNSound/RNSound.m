634134715a8a2d5c43ab39fc7d722d768f526720
Simone Carcone <simonecarcone@gmail.com>
2022-06-21 12:40:04 +0200

    fix remote url

--- RNSound/RNSound.m
+++ RNSound/RNSound.m
@@ -198,7 +198,8 @@ RCT_EXPORT_METHOD(prepare
     NSError *error;
     NSURL *fileNameUrl;
     AVAudioPlayer *player;
-    NSString* fileNameEscaped = [fileName stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    //NSString* fileNameEscaped = [fileName stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    NSString* fileNameEscaped = fileName;
 
     if ([fileNameEscaped hasPrefix:@"http"]) {
         fileNameUrl = [NSURL URLWithString:fileNameEscaped];
